4297dcdc5f22558a8d2fcbd541d67e1ac2fd97da
JD Tadlock <jt7903@gmail.com>
2023-12-19 09:18:01 -0500

    finished why-js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,11 @@
-var jdArray = ['JD', 44, '555 coding st'];
-var jdObj = {
-  name: 'JD',
-  age: 44,
-  address: '555 coding st',
-  hobbies: ['pickleball', 'fishing', {
-    location: 'ATL',
-    type: 'fitness'
-  }],
-  printName: function (str, another) {
-    console.log(this.age, str, another);
+var btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  document.body.classList.toggle('dark');
+
+  if (btn.innerText === 'Dark Mode') {
+    btn.innerText = 'Light Mode';
+  } else {
+    btn.innerText = 'Dark Mode';
   }
-};
-
-
-jdObj.printName(true, 'something');
-
-
-// function someFunc(str, num) {
-
-// }
-
-// someFunc('one', 44);
-
-// undefined.someProp();
-// function someFunc() {
-
-// }
-
-// console.log(jdObj.age);
-// console.log(jdObj['address']);
-
-// console.log(jdObj.hobbies[2].type);
-
-
-// var str = 'some string';
-
-// console.log(jdArray.length);
-// console.log(str.length);
-// // console.log(jdObj.something);
-
-// jdArray.push('another value');
-
-// var newArray = jdArray.concat(['one', 'two']);
-
-
-// console.log(newArray);
-
-// if (true) {
-
-// }
+})
