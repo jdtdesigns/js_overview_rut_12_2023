0bbf07fa6a536cc7e91fc2b761b04083409b5fd9
JD Tadlock <jt7903@gmail.com>
2023-12-20 12:22:48 -0500

    finished modal bubbling example

--- js/script.js
+++ js/script.js
@@ -1,79 +1,15 @@
-// Select the choices div
-var choicesDiv = document.querySelector('.choices');
+var modal = document.querySelector('.modal');
+var submitBtn = document.querySelector('.modal button');
 
-// Function that checks if the button pressed contains the correct answer
-function checkAnswer(eventObj) {
+
+submitBtn.addEventListener('click', function () {
+  console.log('Submit some info');
+});
+
+modal.addEventListener('click', function (eventObj) {
   eventObj.stopPropagation();
-
-  // Grab the targeted element that was clicked
-  var el = eventObj.target;
-
-  // Determine conditionally if the el was a button
-  if (el.tagName === 'BUTTON') {
-    // Store the user's answer
-    var userAnswer = el.innerText;
-
-    // Determine if the user's answer(button text) is equal to the current question's correct answer
-    if (userAnswer === questions[0].correctAnswer) {
-      console.log('correct!');
-      // If incorrect
-    } else {
-      console.log('Wrong!');
-    }
-  }
-}
-
-// Set a click listener on the parent div of all the choice buttons
-choicesDiv.addEventListener('click', checkAnswer);
+});
 
 document.body.addEventListener('click', function () {
-  console.log('clicked');
+  modal.classList.add('hide');
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function addEventListener(eventType, callback) {
-//   if (eventType) {
-//     callback({
-//       target: {
-//         innerText: 'Choice 1'
-//       }
-//     })
-//   }
-// }
-
-// addEventListener('click', function (eventObj) {
-//   console.log(eventObj.target);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (var i = 0; i < btns.length; i++) {
-//   // Add a click listener to the button
-//   // btns[i].addEventListener('click', checkAnswer);
-// }
