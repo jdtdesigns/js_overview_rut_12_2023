9425c1cae258d156cc7704e1e73f9f8bbc64f90a
JD Tadlock <jt7903@gmail.com>
2023-12-21 12:30:00 -0500

    continuing quiz overview

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // Select the choices div
 var choicesDiv = document.querySelector('.choices');
+var startBtn = document.querySelector('#start');
+var startWrap = document.querySelector('.start-wrap');
+var questionWrap = document.querySelector('.question-wrap');
+var timeOutput = document.querySelector('#time-output');
+
 // Store a variable that tracks which question the user is currently on
 var questionIndex = 0;
+// Store a variable that tracks the seconds left
+var time = 60;
+// Store a variable that holds the interval
+var timer;
 
-// console.log(questions.length - 1);
 
 // Function that checks if the button pressed contains the correct answer
 // Utilizing event delegation to capture the button click
@@ -26,6 +34,7 @@ function checkAnswer(eventObj) {
       // If incorrect
     } else {
       console.log('Wrong!');
+      time -= 10;
     }
   }
 }
@@ -47,17 +56,41 @@ function displayQuestion() {
     // Set the text of the button to the current choice string in the loop
     choiceBtn.innerText = currentQuestionObj.choices[i];
     // Output(inject)/append the button to the choices div within our question wrap
-    // Select the choices div
-    var choicesDiv = document.querySelector('.choices');
     // Append the button to the div
     choicesDiv.append(choiceBtn);
   }
 }
 
-displayQuestion();
+// Start the timer countdown and decrease the time variable by one every second until time runs out
+function startCountdown() {
+  // Set the inner text of the timeOuput to say 60 seconds
+  timeOutput.innerText = 'Time: ' + time;
+  // Start an interval that repeats every second(1000ms)
+  // Store the interval to a variable so we can clear or stop it later on
+  timer = setInterval(function () {
+    // Decrease our time variable by one
+    time--;
+    // Set the inner text of the timeOutput element to our time variable value
+    timeOutput.innerText = 'Time: ' + time;
+  }, 1000);
+}
+
+// Show the first quiz question, hide the start wrap and start the timer
+function startQuiz() {
+  // Hide the start wrap
+  startWrap.classList.add('hide');
+  // Show the question wrap
+  questionWrap.classList.remove('hide');
+  // Display the first question
+  displayQuestion();
+  // Start the count down
+  startCountdown();
+}
 
 // Set a click listener on the parent div of all the choice buttons
 choicesDiv.addEventListener('click', checkAnswer);
+// Add a click listener to the start button
+startBtn.addEventListener('click', startQuiz);
 
 
 
